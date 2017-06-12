220da248069115ef25cc61f3fe3e7e99ece743e2
dianix <dianavazquez.dcg@gmail.com>
2017-06-12 10:19:05 -0500

    .getJSON

--- app.js
+++ app.js
@@ -4,8 +4,34 @@ var cargarPagina = function () {
 };
 
 var cargarPersonajes = function () {
-    $.ajax("http://swapi.co/api/people/", {
+    var url = "http://swapi.co/api/people/";
+    $.getJSON(url, function (response) {
+        var personajes = response.results;
+        var total = response.count;
+        mostrarTotalPersonajes(total);
+        mostrarPersonajes(personajes);
+    })
+};
 
+var mostrarTotalPersonajes = function (total) {
+    $("#total").text(total)
+};
+
+var mostrarPersonajes = function (personajes) {
+    var $ul = $("#personajes");
+    personajes.forEach(function (personaje) {
+        var $li = $("<li/>");
+        $li.text(personaje.name + " - " + personaje.height + " cm");
+        $ul.append($li)
+        console.log(personaje.name)
+    })
+};
+
+$(document).ready(cargarPagina);
+
+/*var cargarPersonajes = function () {
+    //$.ajax sirve para hacer diversos tipos de peticiones .get es uno específico
+    $.ajax("http://swapi.co/api/people/", {
         method: "GET",
         dataType: "json",
         success: function (response) {
@@ -19,20 +45,4 @@ var cargarPersonajes = function () {
             //console.log("error",error)
         }
     })
-}
-
-var mostrarTotalPersonajes = function(total){
-    $("#total").text(total)
-}
-
-var mostrarPersonajes = function(personajes) {
-    var $ul = $("#personajes");
-    personajes.forEach(function (personaje) {
-        var $li = $("<li/>");
-        $li.text(personaje.name + " - " + personaje.height + " cm");
-        $ul.append($li)
-        console.log(personaje.name)
-    })
-}
-
-$(document).ready(cargarPagina);
+}*/
